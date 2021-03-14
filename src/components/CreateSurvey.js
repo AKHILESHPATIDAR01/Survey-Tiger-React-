c388818940640bd58a0edce96c0d17473332b587
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-03-14 12:44:53 +0530

    Update CreateSurvey.js

--- src/components/CreateSurvey.js
+++ src/components/CreateSurvey.js
@@ -1,91 +1,100 @@
-import Questions from "./Questions"
-import TypeSelector from "./TypeSelector"
+import Questions from "./Questions";
+import TypeSelector from "./TypeSelector";
 import Options from "./Options";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-const CreateSurvey = ( { setSquestions, squestions } ) => {
+const CreateSurvey = ({ setSquestions, squestions }) => {
+  const getRandom = () => {
+    return Math.floor(Math.random() * 1000 + 1);
+  };
 
-    const getRandom = () => { return Math.floor((Math.random() * 1000)+1);}
-    const [qText, setQtext] = useState('');
-    const [qType, setQtype] = useState(0);
-    const [options, setOptions] = useState([{uid: getRandom(), value:''},{uid: getRandom(), value:''}])
+  const history = useHistory();
+  const defaultOptionState = [
+    { uid: getRandom(), value: "" },
+    { uid: getRandom(), value: "" }
+  ];
+  const [qText, setQtext] = useState("");
+  const [qType, setQtype] = useState(0);
+  const [options, setOptions] = useState(defaultOptionState);
 
-    const updateSurveyQuestion = () =>{
-        let newSurveyQuestion = [...squestions];
-        let newQ = {
-            qtext : qText,
-            qtype : qType,
-            optionS : options,
-        }
-        newSurveyQuestion.push(newQ);
-        setSquestions(newSurveyQuestion);
+  const updateSurveyQuestion = () => {
+    let newSurveyQuestion = [...squestions];
+    let newQ = {
+      qtext: qText,
+      qtype: qType,
+      options: options
+    };
+    newSurveyQuestion.push(newQ);
+    setSquestions(newSurveyQuestion);
+    setQtype(0);
+    setQtext("");
+    setOptions(defaultOptionState);
+  };
 
+  const publish = () => {
+    updateSurveyQuestion();
+    history.push("/published");
+  };
+
+  const addOptions = () => {
+    let newOptions = {
+      uid: getRandom(),
+      value: ""
+    };
+
+    let updatedOptions = [...options];
+    updatedOptions.push(newOptions);
+    setOptions(updatedOptions);
+  };
+
+  const deleteOptions = () => {
+    if (options.length === 2) {
+      alert("Error: A select option should have atlest two options ");
+    } else {
+      let updatedOptions = [...options];
+      updatedOptions.pop();
+      setOptions(updatedOptions);
     }
+  };
 
-    const addOptions = () => {
-        
-        let newOptions = {
-            uid: getRandom(),
-            value: ''
-        }
+  const updatedOptionText = (id, text) => {
+    let updateOptions = [...options];
+    let changeIndex = updateOptions.findIndex((x) => x.uid === id);
+    updateOptions[changeIndex].value = text;
+    setOptions(updateOptions);
+  };
 
-        let updatedOptions = [...options];
-        updatedOptions.push(newOptions);
-        setOptions(updatedOptions);
+  return (
+    <>
+      <TypeSelector qType={qType} setQtype={setQtype} />
 
-    }
-
-
-    const deleteOptions = () => {
-        
-        if(options.length === 2){
-            alert("Error: A select option should have atlest two options ");
-        }
-        else{
-            let updatedOptions = [...options];
-            updatedOptions.pop();
-            setOptions(updatedOptions);
-        }
-
-
-    }
-
-    const updatedOptionText = (id,text) => {
-        let updateOptions = [...options];
-        let changeIndex = updateOptions.findIndex(x => x.uid === id);
-        updateOptions[changeIndex].value = text;
-        setOptions(updateOptions);
-    }
-
-
-    return(
+      {qType !== 0 ? (
         <>
-           <TypeSelector qType={qType} setQtype={setQtype} />
-           
-            { qType !== 0 ? 
-                <>
-                    <Questions onTextUpdate={setQtext} />
-                    {options.map((opt, key) => (
-                        <Options 
-                            key={key}
-                            uid={opt.uid}
-                            addOptions = {addOptions}
-                            deleteOptions = {deleteOptions}
-                            updateText = {updatedOptionText}
-                        />
-                    ))}
+          <Questions onTextUpdate={setQtext} />
+          {options.map((opt, key) => (
+            <Options
+              key={key}
+              uid={opt.uid}
+              addOptions={addOptions}
+              deleteOptions={deleteOptions}
+              updateText={updatedOptionText}
+            />
+          ))}
 
-                    <button className="btn btn-primary my-3" onClick={()=>updateSurveyQuestion()} >Add Question</button>
-                    <button className="btn btn-primary my-3" >Publish</button>
-                   
-                </>
-
-            : null}
-
-            
-
+          <button
+            className="btn btn-primary my-3"
+            onClick={() => updateSurveyQuestion()}
+          >
+            Add Question
+          </button>
+          <button className="btn btn-primary my-3" onClick={() => publish()}>
+            Publish
+          </button>
         </>
-    )
-}
+      ) : null}
+    </>
+  );
+};
 
 export default CreateSurvey;
